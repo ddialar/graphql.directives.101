cd09edded1a984c5048004ea32d006bae6e47234
ddialar <ddialar@gmail.com>
2019-05-17 15:28:49 +0100

    chore(server.js): Adapt the server configuration in order to work with directives and context.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import * as path        from 'path';
 import { ApolloServer } from 'apollo-server';
 import { importSchema } from 'graphql-import';
 
+import * as directives  from './graphql/directives';
 import * as resolvers   from './graphql/resolvers';
 
 const typeDefs = importSchema(path.join(__dirname, 'graphql/schema.graphql'));
@@ -9,6 +10,12 @@ const typeDefs = importSchema(path.join(__dirname, 'graphql/schema.graphql'));
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    schemaDirectives: {
+        auth: directives.AuthDirective
+    },
+    context: ({ req }) => ({
+        token: req.headers.authorization
+    }),
     playground: true,
     introspection: true
 });
